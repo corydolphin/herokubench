dc97e887b8f667fc1999b1074fc24dad627dfa55
Cory Dolphin <wcdolphin@gmail.com>
2013-06-03 23:56:40 -0400

    Working prototype using server role

--- lib/herokubench/cli.rb
+++ lib/herokubench/cli.rb
@@ -3,6 +3,7 @@ require "heroku/auth"
 require "heroku/command"
 require "heroku/command/base"
 require "heroku/command/help"
+require "heroku/command/apps"
 require "heroku/cli"
 require "heroku/plugin"
 require "thor"
@@ -11,18 +12,27 @@ require "uri"
 require "herokubench"
 require "yaml"
 require "pathname"
+require "stringio"
 
 class HerokuBench::CLI < Thor
+
+    Heroku.user_agent = "heroku-gem/#{Heroku::VERSION} (#{RUBY_PLATFORM}) ruby/#{RUBY_VERSION}"
+    Heroku::Command.load
 
   desc "create APP_NAME", <<-DESC
 create a bench-server on Heroku
 
   DESC
 
-  def create(name)
+  def create(name="")
     Dir.mktmpdir do |dir|
       Dir.chdir(dir) do
-        system "heroku create #{name} -s cedar --buildpack https://github.com/wcdolphin/heroku-buildpack-apache.git"
+
+        args = ["#{name}", "-s","cedar", "--buildpack","https://github.com/wcdolphin/heroku-buildpack-apache.git"]
+        args.delete("")
+        result = capture { Heroku::Command.run("create", args) }
+        name = /\s(.+)\.\.\./.match(result).captures[0]
+        puts "Creatied app: #{name}"
       end
     end
     write_config :app => name, :host => "#{name}.herokuapp.com"
@@ -40,47 +50,36 @@ update the bench-server
 
     Dir.mktmpdir do |dir|
       Dir.chdir(dir) do
-        system "git init"
+        system "git init -q"
         system "git remote add origin git@github.com:wcdolphin/heroku-benchserver.git"
         system "git remote add heroku git@#{heroku_git_domain}:#{config[:app]}.git"
-        system "git pull origin master >" + null_dev
-        # system "git add . >" + null_dev
-        # system "git commit -m commit >" + null_dev
-        system "git push heroku -f master >" +  null_dev
-        # heroku "config:add LD_LIBRARY_PATH=/app/vendor/apache-2.4.4/lib/:$LD_LIBRARY_PATH BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git"
-        # heroku "config:set PATH=/app/vendor/apache-2.4.4/bin/:$PATH"
+        pullres = capture { system "git pull --quiet origin master"}
+        pushres = capture { system "git push heroku master --quiet"}
       end
     end
   end
 
-  desc "bench site", "bench the bench-server"
+  desc "ab site", "run apache-bench, from the cloud!"
   method_option :concurrency , :aliases => "-c", :default => 1000, :desc => "Number of multiple requests to perform at a time. Default is one request at a time."
   method_option :requests , :aliases => "-n", :default => 10000, :desc => "Number of requests to perform for the benchmarking session"
   method_option :instances, :alias => "-i", :default => 2, :desc => "Number of instances to run simultaneously, default is 1"
-  def bench(site, c=1000, n=10000, i=1)
+  def ab(site, c=1000, n=10000, i=1)
     error "no app yet, create first" unless config[:app]
-
-
-    Heroku.user_agent = "heroku-gem/#{Heroku::VERSION} (#{RUBY_PLATFORM}) ruby/#{RUBY_VERSION}"
-    Heroku::Command.load
-    Heroku::Command.run("run", ["ab -c #{options[:concurrency]} -n #{options[:requests]} #{site}", "--app", "#{config[:app]}"])
-
-
-    # Heroku.user_agent = "heroku-gem/#{Heroku::VERSION} (#{RUBY_PLATFORM}) ruby/#{RUBY_VERSION}"
-    # Heroku::Command.load
-    # Heroku::Command.run("run", ["ab -c #{c} -n #{n} #{site}", "--app", "#{config[:app]}"])
-
-    # Heroku::CLI.start("run", "ab -c 1000 -n 1000 http://www.google.com/", "--app", "#{config[:app]}")
-
+    puts "Running one-off dyno, please be patient"
+    puts capture { Heroku::Command.run("run", ["ab -c #{options[:concurrency]} -n #{options[:requests]} #{site}", "--app", "#{config[:app]}"])}
   end
-
-   desc "hello NAMES", "long desc"
-    def hello(*names)
-        puts "hello #{names.join('; ')}"
-    end
 
 private
  
+  def capture
+    results = $stdout = StringIO.new
+    yield
+    $stdout = STDOUT
+    results.close_write
+    results.rewind
+    return results.read
+  end
+
  #Yeah, we are windows compatible. Because you should be too.
  def null_dev
     return test(?e, '/dev/null') ? '/dev/null' : 'NUL:'
